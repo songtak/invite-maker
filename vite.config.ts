f824040e386e78e372f7950ac4949bf7f0ceee86
songtak <missong3@naver.com>
2024-11-24 20:08:59 +0900

    dd

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "www.emoji2025.site",
+  // base: "www.emoji2025.site",
   // base: "https://songtak.github.io/invite-maker",
   plugins: [react()],
   resolve: {
