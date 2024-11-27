dc5dc9612354f06068d44aa1362c5e06f48ecb41
songtak <missong3@naver.com>
2024-11-27 16:08:40 +0900

    메타태그 설정

--- src/components/TypingEffect.tsx
+++ src/components/TypingEffect.tsx
@@ -16,36 +16,25 @@ interface TypingEffectProps {
 const TypingEffect: React.FC<TypingEffectProps> = ({ data, onComplete }) => {
   const [typedText, setTypedText] = useState<string[]>([]);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [charIndex, setCharIndex] = useState(0);
 
   useEffect(() => {
     if (currentIndex < data.length) {
-      const description = data[currentIndex]?.description || "";
-      if (charIndex < description.length) {
-        // 타이핑 효과
-        const timer = setTimeout(() => {
-          setTypedText((prev) => {
-            const updatedText = [...prev];
-            updatedText[currentIndex] =
-              (updatedText[currentIndex] || "") + description[charIndex];
-            return updatedText;
-          });
-          setCharIndex((prev) => prev + 1);
-        }, 30); // 타이핑 속도 (밀리초)
-        return () => clearTimeout(timer);
-      } else {
-        // 다음 항목으로 넘어가기 전에 상태 초기화
-        const nextTimer = setTimeout(() => {
-          setCurrentIndex((prev) => prev + 1);
-          setCharIndex(0);
-        }, 200); // 항목 간 대기 시간 (밀리초)
-        return () => clearTimeout(nextTimer);
-      }
+      const description = data[currentIndex].description;
+
+      // 현재 항목의 description을 전체적으로 출력
+      setTypedText((prev) => [...prev, description]);
+
+      // 다음 항목으로 넘어가기 전 2초 대기
+      const timer = setTimeout(() => {
+        setCurrentIndex((prev) => prev + 1);
+      }, 800); // 각 description 간 대기 시간 (밀리초)
+
+      return () => clearTimeout(timer);
     } else if (onComplete) {
       // 모든 타이핑이 끝났을 때 onComplete 호출
       onComplete();
     }
-  }, [charIndex, currentIndex, data, onComplete]);
+  }, [currentIndex, data, onComplete]);
 
   const isMobile = () => {
     return /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(
@@ -53,10 +42,29 @@ const TypingEffect: React.FC<TypingEffectProps> = ({ data, onComplete }) => {
     );
   };
 
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    if (activeIndex < typedText.length) {
+      const timer = setTimeout(() => {
+        setActiveIndex(activeIndex + 1);
+      }, 400); // 각 텍스트의 애니메이션 간격
+
+      return () => clearTimeout(timer);
+    }
+  }, [activeIndex, typedText.length]);
+
   return (
-    <div>
+    <div className="">
       {typedText.map((text, index) => (
-        <div key={index} className={`lh ${isMobile() ? "pb24" : "pb36"}`}>
+        <div
+          key={index}
+          className={`lh ${isMobile() ? "pb24" : "pb36"} fade-in-slide-down ${
+            index < activeIndex ? "active" : ""
+          }`}
+
+          // className={`lh ${isMobile() ? "pb24" : "pb36"}`}
+        >
           {text}
         </div>
       ))}
@@ -65,3 +73,71 @@ const TypingEffect: React.FC<TypingEffectProps> = ({ data, onComplete }) => {
 };
 
 export default TypingEffect;
+
+// import React, { useState, useEffect } from "react";
+
+// interface Emoji {
+//   id: number;
+//   emoji: string;
+//   title: string;
+//   symbol: string;
+//   description: string;
+// }
+
+// interface TypingEffectProps {
+//   data: Emoji[];
+//   onComplete?: () => void; // 모든 타이핑이 끝났음을 알리는 콜백
+// }
+
+// const TypingEffect: React.FC<TypingEffectProps> = ({ data, onComplete }) => {
+//   const [typedText, setTypedText] = useState<string[]>([]);
+//   const [currentIndex, setCurrentIndex] = useState(0);
+//   const [charIndex, setCharIndex] = useState(0);
+
+//   useEffect(() => {
+//     if (currentIndex < data.length) {
+//       const description = data[currentIndex]?.description || "";
+//       if (charIndex < description.length) {
+//         // 타이핑 효과
+//         const timer = setTimeout(() => {
+//           setTypedText((prev) => {
+//             const updatedText = [...prev];
+//             updatedText[currentIndex] =
+//               (updatedText[currentIndex] || "") + description[charIndex];
+//             return updatedText;
+//           });
+//           setCharIndex((prev) => prev + 1);
+//         }, 50); // 타이핑 속도 (밀리초)
+//         return () => clearTimeout(timer);
+//       } else {
+//         // 다음 항목으로 넘어가기 전에 상태 초기화
+//         const nextTimer = setTimeout(() => {
+//           setCurrentIndex((prev) => prev + 1);
+//           setCharIndex(0);
+//         }, 200); // 항목 간 대기 시간 (밀리초)
+//         return () => clearTimeout(nextTimer);
+//       }
+//     } else if (onComplete) {
+//       // 모든 타이핑이 끝났을 때 onComplete 호출
+//       onComplete();
+//     }
+//   }, [charIndex, currentIndex, data, onComplete]);
+
+//   const isMobile = () => {
+//     return /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(
+//       navigator.userAgent
+//     );
+//   };
+
+//   return (
+//     <div>
+//       {typedText.map((text, index) => (
+//         <div key={index} className={`lh ${isMobile() ? "pb24" : "pb36"}`}>
+//           {text}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default TypingEffect;
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { ViteFaviconsPlugin } from "vite-plugin-favicon";
 import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: "www.emoji2025.site",
   // base: "https://songtak.github.io/invite-maker",
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteFaviconsPlugin({
+      logo: "public/logo.png",
+    }),
+  ],
   resolve: {
     alias: [
       { find: "@", replacement: path.resolve(__dirname, "/src") },
